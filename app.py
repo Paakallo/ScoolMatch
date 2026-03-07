1f37d1ddb87e583891e77976cfbc8717b9138ca4
Paakallo <paviveerar@gmail.com>
2026-03-07 22:53:43 +0100

    fix js module and template folder

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Wskazujemy Flaskowi poprawny folder z szablonami
+app = Flask(__name__, template_folder='website/templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Kiedy JS robi fetch('QA.html'), Flask przechwytuje to tutaj
+@app.route('/<module_name>')
+def serve_module(module_name):
+    # Sprawdzamy, czy plik kończy się na .html, żeby było bezpieczniej
+    if module_name.endswith('.html'):
+        return render_template(module_name)
+    return "Nie znaleziono", 404
+
 if __name__ == '__main__':
-    # Tryb debug = True jest idealny na hackathonie, bo odświeża apkę po zapisaniu pliku
     app.run(debug=True)
